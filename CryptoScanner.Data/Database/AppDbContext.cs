6c354cfd81e88d2083dac29621229af5f9f065a8
Oguzcan Avci <Oguzcan.avci95@gmail.com>
2024-02-12 16:01:27 +0100

    Add coinmodel and methods

---
+++ CryptoScanner.Data/Database/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CryptoScanner.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoScanner.Data.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<CoinModel> Coins { get; set; }
+    }
+}
---
+++ CryptoScanner.Data/Services/CoinRepository.cs
@@ -0,0 +1,30 @@
+﻿using CryptoScanner.Data.Database;
+using CryptoScanner.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoScanner.Data.Services
+{
+    public class CoinRepository : ICoinRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CoinRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<CoinModel> GetCoins()
+        {
+            return _context.Coins;
+        }
+
+        public CoinModel? GetCoin(string coinName)
+        {
+            return _context.Coins.FirstOrDefault(c => c.Name == coinName);
+        }
+    }
+}
